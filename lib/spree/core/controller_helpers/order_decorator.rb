66cd4b387690d8b4b03e67a7cc1cdb373cd677a0
stefan <stefan@yo-code.de>
2019-12-01 01:27:08 +0100

    let not interfere current_currency with APIs getting state list

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -2,10 +2,12 @@ Spree::Core::ControllerHelpers::Order.class_eval do
   def current_currency
     # ensure session currency is supported
     #
-    if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
-      session[:currency]
-    else
-      Spree::Config[:currency]
+    unless request.xhr? # does interfere with APIs getting state list
+      if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+        session[:currency]
+      else
+        Spree::Config[:currency]
+      end
     end
   end
 end
